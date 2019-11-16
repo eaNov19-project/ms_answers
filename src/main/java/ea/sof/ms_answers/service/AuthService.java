3c15ba561ac7c1b1ac6fa731d80322928669677a
thanhtuyenle <ttle@mum.edu>
2019-11-16 17:43:58 -0600

    commit change auth url

--- src/main/java/ea/sof/ms_answers/service/AuthService.java
+++ src/main/java/ea/sof/ms_answers/service/AuthService.java
@@ -3,7 +3,7 @@ package ea.sof.ms_answers.service;
 import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "authms", url = "http://104.154.33.123:8080/auth")
+@FeignClient(name = "${feign.name}", url = "${feign.url}")
 public interface AuthService extends MsAuthShowcase {
 
 }
