7f872d06eafdefb9aa93204aa4417cbda0bd49f3
thanhtuyenle <ttle@mum.edu>
2019-11-17 12:42:52 -0600

    fixing bugs

--- src/main/java/ea/sof/ms_answers/service/AuthService.java
+++ src/main/java/ea/sof/ms_answers/service/AuthService.java
@@ -3,7 +3,8 @@ package ea.sof.ms_answers.service;
 import ea.sof.shared.showcases.MsAuthShowcase;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "${feign.name}", url = "${feign.url}")
+//@FeignClient(name = "${feign.name}", url = "${feign.url}")
+@FeignClient(name="authService", url = "${AUTHENTICATE_SERVICE}")
 public interface AuthService extends MsAuthShowcase {
 
 }
